445485be4f393546baab1e778957794d221fdc61
SnoWxK <2628590807@qq.com>
2026-02-26 09:32:03 +0800

    1404.将二进制表示减到1的步骤数

---
+++ 26-02/26-02-26/numSteps.java
@@ -0,0 +1,17 @@
+import java.math.BigInteger;
+
+public class numSteps {
+    public int numSteps(String s){
+        BigInteger num = new BigInteger(s, 2);
+        int count = 0;
+        while (!num.equals(BigInteger.ONE)) {
+            if (num.remainder(BigInteger.valueOf(2)).equals(BigInteger.ZERO)) {
+                num = num.divide(BigInteger.valueOf(2));
+            } else {
+                num = num.add(BigInteger.ONE);
+            }
+            count++;
+        }
+        return count;
+    }
+}
