29826a1b50ef803ab064a983c809bfcaac41756e
SnoWxK <2628590807@qq.com>
2026-03-18 09:47:44 +0800

    3070.元素和小于等于k的子矩阵的数目

--- 26-03/26-03-18/countSubmatrices.java
+++ 26-03/26-03-18/countSubmatrices.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
 public class countSubmatrices {
+    public int countSubmatrices(int[][] grid, int k) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int count = 0;
+        int[] colSum = new int[cols];
+
+        for (int i = 0; i < rows; i++) {
+            int rowAndColSum = 0;
+            for (int j = 0; j < cols; j++) {
+                colSum[j] += grid[i][j];
+                rowAndColSum += colSum[j];
+                if (rowAndColSum <= k) {
+                    count++;
+                } else {
+                    break;
+                }
+            }
+        }
+        return count;
+    }
 }
