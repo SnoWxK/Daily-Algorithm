7474b09760ef1e7f4c4c6077062c0f45ea788b60
SnoWxK <2628590807@qq.com>
2026-03-24 14:07:51 +0800

    2906.构造乘积矩阵

--- 26-03/26-03-24/constructProductMatrix.java
+++ 26-03/26-03-24/constructProductMatrix.java
@@ -1,9 +1,9 @@
 public class constructProductMatrix {
-    public int[][] constructProductMatrix(int[][] grid) {
+    public int[][] constructProductMatrix(int[][] matrix) {
         int MOD = 12345;
 
-        int n = grid.length;
-        int m = grid[0].length;
+        int n = matrix.length;
+        int m = matrix[0].length;
         int[][] p = new int[n][m];
 
         long pre = 1;
@@ -11,7 +11,7 @@ public class constructProductMatrix {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 p[i][j] = (int) pre;
-                pre = (pre * (grid[i][j] % MOD)) % MOD;
+                pre = (pre * (matrix[i][j] % MOD)) % MOD;
             }
         }
 
@@ -19,7 +19,7 @@ public class constructProductMatrix {
         for (int i = n - 1; i >= 0; i--) {
             for (int j = m - 1; j >= 0; j--) {
                 p[i][j] = (int) ((p[i][j] * suf) % MOD);
-                suf = (suf * (grid[i][j] % MOD)) % MOD;
+                suf = (suf * (matrix[i][j] % MOD)) % MOD;
             }
         }
 
