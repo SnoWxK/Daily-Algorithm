b1b0705ef00b9dda213b0832f75d5cc732f2a823
SnoWxK <2628590807@qq.com>
2026-03-27 08:36:52 +0800

    2946.循环移位后的矩阵相似检查

---
+++ 26-03/26-03-27/areSimilar.java
@@ -0,0 +1,17 @@
+public class areSimilar {
+    public boolean areSimilar(int[][] mat, int k) {
+        int m = mat.length;
+        int n = mat[0].length;
+        k = k % n;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] != mat[i][(j + k + n) % n]) {
+                    return false;
+                }
+            }
+            k *= -1;
+
+        }
+        return true;
+    }
+}
