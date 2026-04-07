f97e3c7ba2e85d2df51ee4e96d5514f21e704352
SnoWxK <2628590807@qq.com>
2026-04-07 11:44:31 +0800

    2069.模拟行走机器人II

---
+++ 26-04/26-04-07/Robot.java
@@ -0,0 +1,38 @@
+class Robot {
+    private int width, height, perimeter;
+    private int pos = 0;
+    private boolean moved = false;
+    private String[] directions = {"East", "North", "West", "South"};
+
+    public Robot(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.perimeter = 2 * (width + height - 2);
+    }
+
+    public void step(int num) {
+        moved = true;
+        pos = (pos + num) % perimeter;
+    }
+
+    public int[] getPos() {
+        if (pos < width) {
+            return new int[]{pos, 0};
+        } else if (pos < width + height - 1) {
+            return new int[]{width - 1, pos - (width - 1)};
+        } else if (pos < 2 * width + height - 2) {
+            return new int[]{width - 1 - (pos - (width + height - 2)), height - 1};
+        } else {
+            return new int[]{0, height - 1 - (pos - (2 * width + height - 3))};
+        }
+    }
+
+    public String getDir() {
+        if (!moved && pos == 0) return "East";
+
+        if (pos > 0 && pos < width) return "East";
+        if (pos >= width && pos < width + height - 1) return "North";
+        if (pos >= width + height - 1 && pos < 2 * width + height - 2) return "West";
+        return "South";
+    }
+}
