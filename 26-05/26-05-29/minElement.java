c150fc898b744ee74f811c92c9e2e08383310540
SnoWxK <2628590807@qq.com>
2026-05-29 08:42:28 +0800

    3300.替换为位数和以后的最小元素

---
+++ 26-05/26-05-29/minElement.java
@@ -0,0 +1,15 @@
+public class minElement {
+    public int minElement(int[] nums) {
+        int ans = Integer.MAX_VALUE;
+        for (int i : nums) {
+            int sum = 0;
+            while (i > 0) {
+                sum += i % 10;
+                i /= 10;
+            }
+            ans = Math.min(ans, sum);
+        }
+        return ans;
+    }
+
+}
